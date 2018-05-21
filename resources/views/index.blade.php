80326870172d250f475d0f6ad5b597e129513ab4
Doder <aleksadoderovic@gmail.com>
2018-05-21 17:48:03 +0200

    add basic layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prodavnica</title>
-</head>
-<body>
-    <ul>
+@extends('layouts.layout')
+@section('content')
     @foreach ($majice as $majica)
         <li><a href="majice/{{$majica->id}}">{{ $majica->naziv }}</a></li>
     @endforeach
     </ul>
-</body>
-</html>
+@endsection
