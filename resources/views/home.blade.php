72deee8c79c7b62884d744a16e04a06991b46058
tatjanavucinic <tastaturaej@gmail.com>
2018-05-26 02:07:13 +0200

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -26,112 +26,164 @@
     <div class="row">
             <div id="owl-carousel" class="owl-carousel owl-theme">
 				
-                    <div class="owl__item"> <img src="img/item1.png"> 
-                          <div class="owl__item__div container">
+                    <div class="owl__item"> <img src="img/item1.jpg"> 
+                          <div class=" container">
                               <div class="row">
-                                  <div class="col-md-10 col content-center">
-                                      <h1>MADE FOR DESIGNERS</h1>
-                                      <h4>Create your online portfolio in minutes  with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.
+                                  <div class="col-md-10">
+                                      <h1>NAPRAVLJENO ZA SVAKOGA</h1>
+                                      <h4>Napravite svoju zeljenu majicu u samo par minuta, ili izaberite neku iz nase ponude!
                                       </h4>
                                   </div>
                               </div>
                               <div class="row">
-                                  <div class="col-md-3 content-center">
-                                      <button type="button" class="btn btn-block btn-lg slajd_dugme">GET STARTED NOW</button>
+                                  <div class="col-md-3">
+                                      <button type="button" class="btn btn-block btn-lg slajd_dugme">USUDI SE</button>
                                   </div>
                               </div>
                         </div>
                     </div>
                     
-                    <div class="owl__item"> <img src="img/item2.png">
-                        <div class="owl__item__div container">
+                    <div class="owl__item"> <img src="img/item2.jpg">
+                        <div class=" container">
                             <div class="row">
                                 <div class="col-md-5 col-md-offset-7">
-                                    <h1 class="text-right">FRESH AND</h1>
+                                    <h1 class="text-right">BUDI IN</h1>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-8 col-md-offset-4">
-                                    <h1 class="text-right">BEAUTIFUL DESIGN</h1>
+                                    <h1 class="text-right">PRATI KORAK</h1>
                                     <div>
-                                        <h4 class="text-right">REEN is designed to showcase your talent and put your work in the forefront.<br>Professionally use of typography and layout that fits your content.
+                                        <h4 class="text-right"> Majice su izuzetnog kvaliteta i raznovrsnog dizajna.
                                         </h4>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-3 col-md-offset-9">
-                                    <button type="button" class="btn btn-block btn-lg slajd_dugme">GET STARTED NOW</button>
+                                    <button type="button" class="btn btn-block btn-lg slajd_dugme">RAZGLEDAJ</button>
                                 </div>
                             </div>
                         </div>
                     </div>
     
-                    <div class="owl__item"> <img src="img/item3.png">
-                        <div class="owl__item__div container">
+                    <div class="owl__item"> <img src="img/item3.jpg">
+                        <div class=" container">
                             <div class="row">
                                 <div class="col-md-6">
-                                    <h1 class="h1__background">CLEAN AND</h1>
+                                    <h1 class="h1__background">CISTA SLIKA</h1>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-7">
-                                    <h1 class="h1__background">REUSABLE CODE</h1>
+                                    <h1 class="h1__background">VRHUNSKI KVALITET</h1>
                                 </div>
                             </div>
                             <div>
-                                <h4 class="div__item--bruise">The clean code allows you to easily copy code blocks from content<br>modules and past them in different places or layouts.</h4>
+                                <h4 class="div__item--bruise">Screen print tehnologija, dugotrajna slika i kvalitet.</h4>
                             </div>
                             <div class="row">
                                 <div class="col-md-3">
-                                    <button type="button" class="btn btn-block btn-lg slajd_dugme">GET STARTED NOW</button>
+                                    <button type="button" class="btn btn-block btn-lg slajd_dugme">RAZGLEDAJ</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 
-                    <div class="owl__item"> <img src="img/item4.png"> 
-                        <div class=" owl__item__div container">
+                    <div class="owl__item"> <img src="img/item4.jpg"> 
+                        <div class="  container">
                             <div class="row">
                                 <div class="col-md-7 col-md-offset-5">
-                                    <h1 class="h1__background">JUST FOCUS ON</h1>
+                                    <h1 class="h1__background">PUSTI MASTU</h1>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-8 col-md-offset-4">
-                                    <h1 class="h1__background">YOUR CREATIVITY</h1>
+                                    <h1 class="h1__background">NA VOLJU</h1>
                                     <div>
-                                        <h4 style="text-align: right;">Take a break from messing around with heavy coding and spend your<br> time brainstorming ideas for your next project.</h4>
+                                        <h4 style="text-align: right;">Imas ideju za svoju majicu? Kontaktiraj nas!</h4>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-3 col-md-offset-9">
-                                    <button type="button" class="btn btn-block btn-lg slajd_dugme">GET STARTED NOW</button>
+                                    <button type="button" class="btn btn-block btn-lg slajd_dugme">KONTAKT</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="owl__item"> <img src="img/item5.png">
-                        <div class="owl__item__div container">
-                            <div class="row">
-                                <div class="col-md-8 content-center">
-                                    <h1>SHOWCASE<br> YOUR CONTENT</h1>
-                                </div>
-                            </div>
-                              <div class="row">
-                                  <div class="text-center ">
-                                      <h4 class="div__item--grey">With REEN you have the possibility to create websites for various contents<br> quickly and easily. Now it's up to 	you!
-                                      </h4>
-                                  </div>
-                                  <div class="col-md-3 content-center">
-                                    <button type="button" class="btn btn-block btn-lg slajd_dugme">GET STARTED NOW</button>
-                                </div>
-                            </div>
-                          </div>
-                    </div>
                  </div>
     </div>
+     <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <div class="marketing">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-4">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <h2>Heading</h2>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+    
+
 </div>
 
 <script>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,4 @@
-<footer>
-Footer
-</footer>
+<footer class="container">
+    <p class="float-right"><a href="#">Nazad na vrh!</a></p>
+    <p>&copy; 2017-2018 Majica, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </footer>
