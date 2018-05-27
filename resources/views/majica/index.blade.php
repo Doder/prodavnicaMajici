e3a45b2324ed4c4bc408c2415707c7a90d9ce1f8
unknown <aleksadoderovic@gmail.com>
2018-05-27 15:39:31 +0200

    add design for index page

--- resources/views/majica/index.blade.php
+++ resources/views/majica/index.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.master')
 @section('content')
+    <div class="container">
+    <div class="row">
     @foreach ($majice as $majica)
-        <li><a href="majice/{{$majica->id}}">{{ $majica->naziv }}</a></li>
+        
+        <div class="col-3 shirt-box">
+            
+            <div class="image-container">
+            <a href="majice/{{$majica->id}}"><img width="100%" src="/storage/{{$majica->slika}}" alt="{{$majica->naziv}}"></a>
+            </div>
+            <div class="image-title">
+            <h5>{{$majica->naziv}}</h5>
+            </div>
+        </div> 
+
     @endforeach
-    </ul>
+    </div>
+    </div>
 @endsection
